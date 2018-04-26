5d9c938daac9ad86faa702c0b8bb65f3ee459ff5
yutashi <katayama@xshell.io>
2018-04-26 21:51:13 +0900

    Brush up algos

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ def calc_point(r, theta, shift):
 
 if __name__ == "__main__":
     unicornhathd.off()
+    unicornhathd.brightness(0.6)
 
     r = 6
-    theta = 0
+    start_angle = 0
     while True:
-        if theta % 360 == 0:
-            theta = 0
+        if start_angle % 360 == 0:
+            start_angle = 0
         
+        theta = start_angle    
         for unuse in range(15):
             theta += 5
             x, y = calc_point(r, theta, 8)
             i, j = calc_point(r, theta, 7)
-            unicornhathd.set_pixel(x, y, 100, 250, 0)
-            unicornhathd.set_pixel(i, j, 250, 150, 0)
+            unicornhathd.set_pixel(x, y, 200, 50, 0)
+            unicornhathd.set_pixel(i, j, 200, 120, 0)
 
+        start_angle += 30 
         unicornhathd.show()
         time.sleep(0.08)
         unicornhathd.off()
