902355dc8eb629f63133b3c040052f1ed623bc87
yutashi <katayama@xshell.io>
2018-04-27 21:02:38 +0900

    Update algo to reduce flushes

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def calc_point(r, theta, shift):
 
     return x, y
 
+
 if __name__ == "__main__":
     unicornhathd.off()
-    unicornhathd.brightness(0.6)
+    unicornhathd.brightness(0.5)
 
     r = 6
     start_angle = 0
@@ -19,14 +20,19 @@ if __name__ == "__main__":
             start_angle = 0
         
         theta = start_angle    
-        for unuse in range(15):
-            theta += 5
+        for index in range(10):
+            theta -= 10 
             x, y = calc_point(r, theta, 8)
             i, j = calc_point(r, theta, 7)
-            unicornhathd.set_pixel(x, y, 200, 50, 0)
-            unicornhathd.set_pixel(i, j, 200, 120, 0)
 
-        start_angle += 30 
+            if index == 9:
+                unicornhathd.set_pixel(x, y, 0, 0, 0)
+                unicornhathd.set_pixel(i, j, 0, 0, 0)
+            else:
+                unicornhathd.set_pixel(x, y, 200, 50, 0)
+                unicornhathd.set_pixel(i, j, 200, 120, 0)
+                    
+
+        start_angle += 10
         unicornhathd.show()
-        time.sleep(0.08)
-        unicornhathd.off()
+        time.sleep(0.07)
